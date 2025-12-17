9d4a325a536fb65e6bd69070cafac9155bf53978
Cleber <cleber.risu@outlook.com>
2025-12-17 16:20:53 -0300

    Add Models

---
+++ ApiCatalogo/Models/Categoria.cs
@@ -0,0 +1,13 @@
+
+namespace ApiCatalogo.Models;
+
+public class Categoria
+{
+  public int Id { get; set; }
+
+  public string? Nome { get; set; }
+
+  public string? ImagemUrl { get; set; }
+
+  public ICollection<Produto> Produtos { get; set; } = [];
+}
